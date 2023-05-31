ac85c66e86ecf4bfcd9737dff3ef3ba136414c17
Nick Corona <ncorona3@gatech.edu>
2023-05-31 02:40:52 -0400

    refactor

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import smtplib
 import openai
 import os
 import dotenv
+import logging
+
+# Set up logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 def load_env_vars():
@@ -9,46 +15,43 @@ def load_env_vars():
     dotenv.load_dotenv()
 
 
-def get_email_credentials():
-    """Get email credentials from environment variables."""
-    email = os.getenv("EMAIL_ADDRESS")
-    password = os.getenv("EMAIL_PASSWORD")
-    if not email or not password:
-        raise ValueError(
-            "Email address or password not found in environment variables."
-        )
-    return email, password
-
-
-def get_openai_api_key():
-    """Get OpenAI API key from environment variables."""
-    api_key = os.getenv("OPENAI_API_KEY")
-    if not api_key:
-        raise ValueError("OpenAI API key not found in environment variables.")
-    return api_key
+def get_env_var(var_name):
+    """Get a specified environment variable."""
+    var = os.getenv(var_name)
+    if not var:
+        raise ValueError(f"{var_name} not found in environment variables.")
+    return var
 
 
 def generate_compliment(name):
     """Generate a compliment using OpenAI API."""
-    openai.api_key = get_openai_api_key()
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"Write a creative and heartwarming compliment or a nice message for {name}",
-        temperature=0.7,
-        max_tokens=100,
-    )
+    openai.api_key = get_env_var("OPENAI_API_KEY")
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=f"Write a creative and heartwarming compliment or a nice message for {name}",
+            temperature=0.7,
+            max_tokens=100,
+        )
+    except Exception as e:
+        logging.error(f"Error when calling OpenAI API: {e}")
+        raise
     return response.choices[0].text.strip()  # type: ignore
 
 
 def send_email(to_email, subject, body):
     """Send an email using SMTP."""
-    email, password = get_email_credentials()
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(email, password)
-    msg = f"Subject: {subject}\n\n{body}"
-    server.sendmail(email, to_email, msg)
-    server.quit()
+    email = get_env_var("EMAIL_ADDRESS")
+    password = get_env_var("EMAIL_PASSWORD")
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(email, password)
+            msg = f"Subject: {subject}\n\n{body}"
+            server.sendmail(email, to_email, msg)
+    except Exception as e:
+        logging.error(f"Error when sending email: {e}")
+        raise
 
 
 def send_compliment(to_email, name):
