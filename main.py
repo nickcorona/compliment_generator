dc0b6ed549213404fabf766634abc0ee937a8cdd
Nick Corona <ncorona3@gatech.edu>
2023-05-31 07:55:12 -0400

    add random time send

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import logging
 import os
 import random
 import smtplib
+from time import sleep
+from datetime import date, datetime, time
+from random import randint
 
 import dotenv
 import openai
@@ -48,22 +51,17 @@ def generate_compliment(name):
     """Generate a compliment using OpenAI API."""
     openai.api_key = get_env_var("OPENAI_API_KEY")
 
-    # Select a random attribute
     attribute = random.choice(attributes)
     prompt = (
         f"Craft a personalized compliment for {name}, highlighting their {attribute}."
     )
 
-    try:
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            temperature=0.7,
-            max_tokens=100,
-        )
-    except Exception as e:
-        logging.error(f"Error when calling OpenAI API: {e}")
-        raise
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=100,
+    )
 
     return response.choices[0].text.strip()  # type: ignore
 
@@ -74,27 +72,45 @@ def send_email(to_email, subject, body):
     password = get_env_var("EMAIL_PASSWORD")
     smtp_server = get_env_var("SMTP_SERVER")
     smtp_port = int(get_env_var("SMTP_PORT"))  # convert to int
-    try:
-        with smtplib.SMTP(smtp_server, smtp_port) as server:
-            server.starttls()
-            server.login(email, password)
-            msg = f"Subject: {subject}\n\n{body}"
-            server.sendmail(email, to_email, msg)
-    except Exception as e:
-        logging.error(f"Error when sending email: {e}")
-        raise
+
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(email, password)
+        msg = f"Subject: {subject}\n\n{body}"
+        server.sendmail(email, to_email, msg)
 
 
 def send_compliment(to_email, name):
     """Generate a compliment and send it via email."""
-    compliment = generate_compliment(name)
-    subject = "Your Daily Compliment!"
-    body = compliment
-    send_email(to_email, subject, body)
+    try:
+        compliment = generate_compliment(name)
+        subject = "Your Daily Compliment!"
+        body = compliment
+        send_email(to_email, subject, body)
+        logging.info(f"Compliment sent to {to_email} at {datetime.now()}")
+    except Exception as e:
+        logging.error(f"Error occurred: {e}")
+        raise
 
 
 if __name__ == "__main__":
     load_env_vars()
-    to_email = "jessicasarah.christian@gmail.com"
-    name = "Jessica"
+    to_email = get_env_var("TO_EMAIL")
+    name = get_env_var("NAME")
+
+    current_time = datetime.now().time()
+
+    if current_time < time(7, 0):
+        delay = (
+            datetime.combine(date.today(), time(7, 0))
+            - datetime.combine(date.today(), current_time)
+        ).total_seconds()
+    else:
+        delay = 0
+
+    random_seconds = randint(0, 15 * 60 * 60)  # 15 hours = 15*60*60 seconds
+    sleep_seconds = delay + random_seconds
+    logging.info(f"Sleeping for {sleep_seconds} seconds")
+
+    sleep(sleep_seconds)
     send_compliment(to_email, name)
